0d6896bc2ec23c3122cfddd161a72aa5624549ae
AcGunship <pitaamotlatsi@gmail.com>
2026-02-03 17:44:48 +0200

    5th commit

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Masemene Dimpho',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Teboho Mpholo',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Katleho Makoae',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Molete Lipholo',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Khotso Tsatsi',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Palesa Radebe',
+    avatar: 'user-6.jpg',
+  },
+];
